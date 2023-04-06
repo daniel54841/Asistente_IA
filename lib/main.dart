424876c28b7cb9ac5abb5cc1713f7601a8d10d5e
dany <dandkt@users.noreply.github.com>
2023-04-06 14:07:30 +0200

    Actualizado main al paquete getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:reproductor_ia/views/home/home_view.dart';
 import 'package:reproductor_ia/views/login/login_view.dart';
 
 void main() {
@@ -15,9 +16,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginView(),
+      initialRoute: "/login",
+      getPages: [
+        GetPage(
+          name: "/login",
+          page: () => LoginView(),
+        ),
+        GetPage(
+          name: "/home",
+          page: () => HomeView(),
+        )
+      ],
     );
   }
 }
-
-
