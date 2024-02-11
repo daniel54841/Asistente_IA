d0c885173854f0138ba77563bb29436d4c4784f3
Dany <dany54841@gmail.com>
2024-02-11 12:01:18 +0100

    Creada clase de constantes para mensajes genericos

---
+++ lib/utils/constants/general_constants.dart
@@ -0,0 +1,23 @@
+class GeneralConstants {
+  static String email_not_found =
+      "Email incorrecto. No está asociado a ningun correo guardado.\nIntentelo de nuevo";
+
+  static String wrong_password = "Contraseña incorrecta.\nIntentelo de nuevo";
+
+  static String user_disabled = "Cuenta deshabilitada por el administrador.";
+
+  static String generic_error =
+      "Ha ocurrido un error inesperado.\nCompruebe la conexion y vuelve a intentarlo.";
+
+  static String no_internet_connection =
+      "No tienes conexión a internet.\nVuelve a intentarlo más tarde.";
+
+  static String empty_value =
+      "No se ha introducido ningun valor.\nVuelve a intentarlo.";
+
+  static String email_wrong =
+      "Correo introducido no valido.\nVuelve a intentarlo.";
+
+  static String password_wrong =
+      "Contraseña no valida, supera la longitud permitida.\nVuelve a intentarlo.";
+}
