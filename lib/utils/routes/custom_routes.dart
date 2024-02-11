e15c3d1c1acef4870b8eaca8cf177b3fa8b10538
Dany <dany54841@gmail.com>
2024-02-11 13:40:10 +0100

    Añadida nueva ruta

--- lib/utils/routes/custom_routes.dart
+++ lib/utils/routes/custom_routes.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
 
 /*import '../views/home/home_view.dart';*/
-import '../views/home/home_view.dart';
-import '../views/login/login_view.dart';
+import '../../views/home/home_view.dart';
+import '../../views/home/list/list_detail_view.dart';
+import '../../views/login/login_view.dart';
 
 class CustomRoutes {
   static List<GetPage<dynamic>> getListRoutes() {
@@ -14,7 +15,11 @@ class CustomRoutes {
       GetPage(
         name: "/home",
         page: () => const HomeView(),
-      )
+      ),
+      GetPage(
+        name: "/detail_list",
+        page: () => const ListDetailView(),
+      ),
     ];
   }
 }
