945b1fc5843a45ea9b0507b523ee8a5aa354c369
Dany <dany54841@gmail.com>
2024-02-11 13:40:27 +0100

    Desarrollo de UI de home

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:reproductor_ia/utils/constants/home_constants.dart';
+import 'package:reproductor_ia/widgets/item_list_views/buy_item.dart';
+
+import '../../utils/responsive.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Prueba"),
+    Responsive responsive = Responsive.of(context);
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Text("Home"),
+          ],
         ),
-        body: Text("Bienvenido"),
+      ),
+      backgroundColor: Theme.of(context).splashColor,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).splashColor,
+        title: Text(
+          HomeConstants.titleHome,
+          style: TextStyle(
+            fontSize: responsive.dp(5),
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemBuilder: (context, index) {
+              return BuyItem();
+            },
+            itemCount: 2,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+        ),
       ),
     );
   }
