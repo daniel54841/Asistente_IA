8e8fb37e30e359aeee2e378ff3d2388c3c4f040d
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-04-02 17:04:15 +0200

    Actualizacion diseño vista

--- lib/views/list/new_compra.dart
+++ lib/views/list/new_compra.dart
@@ -1,10 +1,51 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:reproductor_ia/controllers/list_buy_item_controller.dart';
+import 'package:reproductor_ia/widgets/TextFormFields/custom_text_form_field.dart';
+import 'package:reproductor_ia/widgets/item_list_views/item_new_producto_busqueda.dart';
 
 class NewCompra extends StatelessWidget {
-  const NewCompra({Key? key}) : super(key: key);
-
+  NewCompra({Key? key}) : super(key: key);
+  final TextEditingController _ctrl = TextEditingController();
+  ListBuyItemController _listCtrl = Get.find<ListBuyItemController>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomTextFormFields(
+          ctrl: _ctrl,
+          titleLabel: "Agregar nuevo producto",
+          getCtrl: _listCtrl,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _listCtrl.cleanOnlyUIItems();
+            },
+            icon: const Icon(
+              Icons.cleaning_services_rounded,
+            ),
+          )
+        ],
+        /*leading: IconButton(
+          icon: const Icon(Icons.check),
+          onPressed: () {},
+        ),*/
+      ),
+      body: GetBuilder(
+        builder: (ListBuyItemController ctrl) {
+          return ctrl.datos.isEmpty
+              ? Container()
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ItemNewProductoBusqueda(
+                      productoBuscado: ctrl.datos[index].producto!,
+                    );
+                  },
+                  itemCount: ctrl.datos.length,
+                );
+        },
+      ),
+    );
   }
 }
